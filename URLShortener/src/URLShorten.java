1421a3151b7b3f1539d640b871830e75ee08b9e5
Steven <sxli@buffalo.edu>
2020-03-20 16:47:02 -0400

    Possible to generate URL

--- URLShortener/src/URLShorten.java
+++ URLShortener/src/URLShorten.java
@@ -1,9 +1,10 @@
 import java.util.HashMap;
+import java.util.Random;
 import java.util.Scanner;
 
 public class URLShorten {
 	HashMap<String, String> hs;
-	
+	private final String ALPHA_NUMERIC_String = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 	public URLShorten() {
 		hs = new HashMap<String, String>();
 	}
@@ -28,14 +29,29 @@ public class URLShorten {
 		}
 	}
 	
+	public String generateURL() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("steven.cc/");
+		Random rand = new Random();
+		for (int i = 0; i < 6; i++) {
+			int r = rand.nextInt(ALPHA_NUMERIC_String.length());
+			sb.append(Character.toString(ALPHA_NUMERIC_String.charAt(r)));
+		}
+		return sb.toString();
+	}
+	
 	public void shortenURL() {
 		URLShorten urlDB = new URLShorten(); 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Please enter the URL you wish to shorten: ");
 		String original = sc.nextLine();		
-		System.out.println("Please enter the shortened link. i.e steven.cc/ : ");
+		System.out.println("Please enter the shortened link. i.e steven.cc/ or type generate for a random link : ");
 		String result = sc.nextLine();
 		while (!result.startsWith("steven.cc/")) {
+			if (result.equals("generate")) {
+				result = urlDB.generateURL();
+				break;
+			}
 			System.out.println("Please enter the shortened link starting with steven.cc/ : ");
 			result = sc.nextLine();
 		}
@@ -46,6 +62,7 @@ public class URLShorten {
 		else {
 			System.out.println("Attempted URL to be shortened to has already been used");
 		} 
+		System.out.println(urlDB.getURL(result));
 		sc.close();
 	}
 	
